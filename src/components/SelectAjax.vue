0078f717cbafb7b94174355f98f2a74a1063a6b3
MarceloHFilho <contato@omarceleza.com.br>
2024-09-02 23:19:18 -0300

    Add execução de enter

--- src/components/SelectAjax.vue
+++ src/components/SelectAjax.vue
@@ -7,6 +7,7 @@
     >
     <select
       @change="$emit('update:modelValue', $event.target.value)"
+      @keydown.enter="$emit('enterPressed')"
       id="countries"
       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
@@ -22,7 +23,7 @@ import api from "../api";
 
 const mensagem = ref("Escolha");
 const items = ref([]);
-defineEmits(["update:modelValue"]);
+defineEmits(["update:modelValue", "enterPressed"]);
 const props = defineProps({
   label: {
     type: String,
