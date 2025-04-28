a52932817156dd5b7934efd49f860375943bea28
MarceloHFilho <contato@omarceleza.com.br>
2025-04-28 15:15:30 -0300

    Removido script padrão

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted } from "vue";
-
-onMounted(() => {
-  const script = document.createElement("script");
-
-  script.src = "https://buttons.github.io/buttons.js";
-  script.async = true;
-
-  document.body.appendChild(script);
-});
-</script>
-
 <template>
   <RouterView />
 </template>
