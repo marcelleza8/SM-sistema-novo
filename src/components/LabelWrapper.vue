712668473cc0280564f9d3e42cdbeeb1d06f7048
MarceloHFilho <contato@omarceleza.com.br>
2025-05-17 20:59:20 -0300

    Alterado ordem das tags

--- src/components/LabelWrapper.vue
+++ src/components/LabelWrapper.vue
@@ -10,7 +10,7 @@ import LabelTag from "./LabelTag.vue";
 import api from "../api";
 
 const tags = ref([]);
-const timerId = ref(0);
+const timerId = ref(0); //
 const loadTags = async () => {
   const response = await api.get(
     `${import.meta.env.VITE_API_FLASK_URL}upload-progress`
